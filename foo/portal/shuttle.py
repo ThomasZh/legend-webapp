fd9d754ac514f4e2f7c1e69037d1b80242ee5e7a
ThomasZhang <thomas.zh@qq.com>
2017-02-04 11:14:05 +0800

    路由

--- foo/portal/shuttle.py
+++ foo/portal/shuttle.py
@@ -36,5 +36,256 @@ from global_const import *
 class ShuttleIndexHandler(tornado.web.RequestHandler):
     def get(self):
         logging.info(self.request)
-
         self.render('shuttle/index.html')
+
+
+class ShuttleIndexSlicedHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/index-sliced.html')
+
+
+class ShuttleIndexSliderHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/index-slider.html')
+
+
+class ShuttleIndexDrawerHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/index-drawer.html')
+
+
+class ShuttleIndexWalkthroughHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/index-walkthrough.html')
+
+
+class Shuttle404Handler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/404.html')
+
+
+class ShuttleActivityHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/activity.html')
+
+
+class ShuttleArticleHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/article.html')
+
+
+class ShuttleBlankHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/blank.html')
+
+
+class ShuttleBlogMasonryHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/blog-masonry.html')
+
+
+class ShuttleBlogHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/blog.html')
+
+
+class ShuttleCalendarHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/calendar.html')
+
+
+class ShuttleCategoryHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/category.html')
+
+
+class ShuttleChartHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/chart.html')
+
+
+class ShuttleChatHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/chat.html')
+
+
+class ShuttleCheckoutHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/checkout.html')
+
+
+class ShuttleComingSoonHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/coming-soon.html')
+
+
+class ShuttleContactHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/contact.html')
+
+
+class ShuttleDualSidebarHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/dual-sidebar.html')
+
+
+class ShuttleEventHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/event.html')
+
+
+class ShuttleFaqHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/faq.html')
+
+
+class ShuttleForgotHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/forgot.html')
+
+
+class ShuttleGalleryCardHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/gallery-card.html')
+
+
+class ShuttleGalleryFilterHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/gallery-filter.html')
+
+
+class ShuttleGalleryMasonryHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/gallery-masonry.html')
+
+
+class ShuttleLeftSidebarHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/left-sidebar.html')
+
+
+class ShuttleLockscreenHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/lockscreen.html')
+
+
+class ShuttleLoginHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/login.html')
+
+
+class ShuttleMaterialHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/material.html')
+
+
+class ShuttleNewsHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/news.html')
+
+
+class ShuttlePlayerHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/player.html')
+
+
+class ShuttlePortfolioCardHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/portfolio-card.html')
+
+
+class ShuttlePortfolioFilterHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/portfolio-filter.html')
+
+
+class ShuttlePortfolioMasonryHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/portfolio-masonry.html')
+
+
+class ShuttleProductHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/product.html')
+
+
+class ShuttleProfileHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/profile.html')
+
+
+class ShuttleProjectHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/project.html')
+
+
+class ShuttleRightSidebarHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/right-sidebar.html')
+
+
+class ShuttleSearchHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/search.html')
+
+
+class ShuttleShopHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/shop.html')
+
+
+class ShuttleSignupHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/signup.html')
+
+
+class ShuttleTimelineHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/timeline.html')
+
+
+class ShuttleTodoHandler(tornado.web.RequestHandler):
+    def get(self):
+        logging.info(self.request)
+        self.render('shuttle/todo.html')
